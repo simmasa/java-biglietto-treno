6da148ce27921dc06fbeb3a4bbc65b6042ddcc03
simmasa <s.masa@hotmail.it>
2022-06-07 16:15:35 +0200

    aggiunt bonus

---
+++ src/jana60bonus/CalcolaBiglietto.java
@@ -0,0 +1,38 @@
+package jana60bonus;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class CalcolaBiglietto {
+
+	public static void main(String[] args) {
+		
+		int anni;
+		double km;
+		
+		Scanner richeste = new Scanner(System.in);
+		
+		System.out.println("Inserisci la tua età: ");
+		anni = richeste.nextInt();
+		
+		System.out.println("Inserisci i kilometri che devi percorrere: ");
+		km = richeste.nextInt();
+		
+		if (km<0 || anni<0 ) {
+			System.out.println("Valori invalidi");
+		} else if (anni < 18) {
+			System.out.print("Il prezzo del biglietto è: ");
+			System.out.print(String.format("%.2f", km*(0.21-(0.21*0.2))));
+			System.out.print("€");
+		} else if (anni >= 65) {
+			System.out.print("Il prezzo del biglietto è: ");
+			System.out.print(String.format("%.2f", km*(0.21-(0.21*0.4))));
+			System.out.print("€");
+		} else {
+			System.out.print("Il prezzo del biglietto è: ");
+			System.out.print(String.format("%.2f", (km*0.21)));
+			System.out.print("€");
+		}
+	}
+
+}
